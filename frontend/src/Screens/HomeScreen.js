6e521caa544cd2ab387528d9fa188c94ca25465b
cah1983 <carolineho1983@gmail.com>
2021-03-09 18:57:03 +0000

    debugged issue with display of product name

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -8,15 +8,18 @@ function HomeScreen(props) {
             data.products.map(product =>
                 <li>
                     <div className="product">
-                        <Link to={'/product/' + product._id}><img className="product-image" src={product.image} alt="product1" /></Link>
+                        <Link to={'/product/' + product._id}><img className="product-image" src={product.image} alt="product1" />
+                        </Link>
                         <div className="product-name">
-                            <Link to={'/product/' + product._id}> {product.name} </Link>
+                            <Link to={'/product/' + product._id}> {product.name} 
+                        </Link>
                         </div>
                         <div className="product-brand">{product.brand}</div>
                         <div className="product-price">${product.price}</div>
                         <div className="product-rating">{product.rating} stars ({product.numReviews} reviews)</div>
                     </div>
-                </li>)
+                </li>
+            )
         }
 
     </ul>
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -3,7 +3,7 @@ import data from '../data';
 
 function ProductScreen(props) {
     console.log(props.match.params.id);
-    const product = data.products.find(elem => elem._id === props.match.params.id);
+    const product = data.products.find(elem => Number(elem._id) === Number(props.match.params.id));
     return <div>
         <h1>{product.name}</h1>
     </div>
